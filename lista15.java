96de4c3725c63a95f709b205e207198e150d1a7c
GuilhermeLima33 <guigui30042008@gmail.com>
2025-09-26 15:15:15 -0300

    Add files via upload

--- lista15.java
+++ lista15.java
@@ -8,8 +8,8 @@ public class lista15 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
-        // ================== EXERCÍCIO 1 ==================
-        System.out.println("===== Exercício 1 - Vetor de frutas =====");
+        
+        
         String[] frutas = new String[5];
         for (int i = 0; i < 5; i++) {
             System.out.print("Digite o nome da fruta " + (i+1) + ": ");
@@ -20,8 +20,8 @@ public class lista15 {
             System.out.println(fruta);
         }
 
-        // ================== EXERCÍCIO 2 ==================
-        System.out.println("\n===== Exercício 2 - Vetor de números inteiros =====");
+        
+       
         int[] numeros10 = new int[10];
         for (int i = 0; i < 10; i++) {
             System.out.print("Digite o número " + (i+1) + ": ");
@@ -32,8 +32,8 @@ public class lista15 {
             System.out.println(n);
         }
 
-        // ================== EXERCÍCIO 3 ==================
-        System.out.println("\n===== Exercício 3 - Ordem inversa =====");
+        
+   
         int[] numeros7 = new int[7];
         for (int i = 0; i < 7; i++) {
             System.out.print("Digite o número " + (i+1) + ": ");
@@ -44,8 +44,8 @@ public class lista15 {
             System.out.println(numeros7[i]);
         }
 
-        // ================== EXERCÍCIO 4 ==================
-        System.out.println("\n===== Exercício 4 - Maiores que 100 =====");
+        
+        
         int[] numeros100 = new int[10];
         for (int i = 0; i < 10; i++) {
             System.out.print("Digite o número " + (i+1) + ": ");
@@ -57,8 +57,8 @@ public class lista15 {
             }
         }
 
-        // ================== EXERCÍCIO 5 ==================
-        System.out.println("\n===== Exercício 5 - Procurando valor no vetor =====");
+       
+      
         int[] numeros8 = new int[8];
         for (int i = 0; i < 8; i++) {
             System.out.print("Digite o número " + (i+1) + ": ");
@@ -79,8 +79,8 @@ public class lista15 {
             System.out.println("Número não existe no vetor!");
         }
 
-        // ================== EXERCÍCIO 6 ==================
-        System.out.println("\n===== Exercício 6 - Nome e idade =====");
+       
+       
         String[] nomes = new String[5];
         int[] idades = new int[5];
         for (int i = 0; i < 5; i++) {
@@ -89,13 +89,13 @@ public class lista15 {
             System.out.print("Digite a idade de " + nomes[i] + ": ");
             idades[i] = sc.nextInt();
         }
-        System.out.println("\nInformações:");
+        System.out.println("Informações:");
         for (int i = 0; i < 5; i++) {
             System.out.println(nomes[i] + " tem " + idades[i] + " anos");
         }
 
-        // ================== EXERCÍCIO 7 ==================
-        System.out.println("\n===== Exercício 7 - Identificação de idade =====");
+        
+        
         int[] idades10 = new int[10];
         for (int i = 0; i < 10; i++) {
             System.out.print("Digite a idade " + (i+1) + ": ");
@@ -107,7 +107,7 @@ public class lista15 {
             }
         }
 
-        // ================== EXERCÍCIO 8 ==================
+        
         System.out.println("\n===== Exercício 8 - Aumento em massa =====");
         double[] salarios = new double[7];
         for (int i = 0; i < 7; i++) {
